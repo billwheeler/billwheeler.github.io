b8af007c04ff25c3f1bae98d7161b6502bd55d54
Bill Wheeler <bwheeler@mathway.com>
2019-06-12 14:57:24 -0400

    added a release build that uglifies js

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,27 @@ const babel = require('gulp-babel')
 const babelify = require('babelify')
 const browserify = require('browserify')
 const source = require('vinyl-source-stream')
+const buffer = require('vinyl-buffer')
+const uglify = require('gulp-uglify')
+
+
+gulp.task('releasejs', () => {
+    browserify({
+        standalone: 'App',
+        entries: './src/main.js',
+        debug: false
+    })
+    .transform(babelify, {
+        presets: ['@babel/preset-env']
+    })
+    .on('error', gutil.log)
+    .bundle()
+    .on('error', gutil.log)
+    .pipe(source('scripts.js'))
+    .pipe(buffer()) 
+    .pipe(uglify()) 
+    .pipe(gulp.dest('js'))
+})
 
 
 gulp.task('debugjs', () => {
