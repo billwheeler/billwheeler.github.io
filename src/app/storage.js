5b1f15bf493502cfcd188c6792abd79ea3a5b3d9
Bill Wheeler <bwheeler@mathway.com>
2020-01-10 10:22:49 -0500

    starting updates for session 10

--- src/app/storage.js
+++ src/app/storage.js
@@ -1,7 +1,7 @@
 ﻿'use strict'
 
 const axios = require('axios')
-const storageKey = 'OssariaSessionNine'
+const storageKey = 'OssariaSessionTen'
 
 var save = (data) => localStorage.setItem(storageKey, data)
 
