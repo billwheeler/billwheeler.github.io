1bc973a1a87c9140f4d489ae024b0f23f08fc1ab
Bill Wheeler <bwheeler@mathway.com>
2020-01-13 09:40:41 -0500

    stuff for eleven

--- src/app/storage.js
+++ src/app/storage.js
@@ -1,7 +1,7 @@
 ﻿'use strict'
 
 const axios = require('axios')
-const storageKey = 'OssariaSessionTen'
+const storageKey = 'OssariaSessionEleven'
 
 var save = (data) => localStorage.setItem(storageKey, data)
 
